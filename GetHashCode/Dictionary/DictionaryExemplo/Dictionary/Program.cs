851484c49e58f65dde52af2b4e7329fd7c25a97e
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-02-14 16:02:44 -0300

    exemplos de GetHashCode

---
+++ GetHashCode/Dictionary/DictionaryExemplo/Dictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace DictionaryExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "Ryan";
+            cookies["email"] = "ryan@gmail.com";
+            cookies["phone"] = "95109-7336";
+            cookies["phone"] = "98478-9551";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("A chave 'email' não existe.");
+            }
+
+            Console.WriteLine("Tamanho: " + cookies.Count);
+
+            Console.WriteLine("Todos os cookies");
+            foreach (var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
---
+++ GetHashCode/Dictionary/ExDictionary/ExDictionary/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com o caminho do arquivo: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+                Console.WriteLine();
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+                    Console.WriteLine("Numero de votos:");
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
