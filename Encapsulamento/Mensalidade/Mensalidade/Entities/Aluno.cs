3ee74fe37d53244b6073bc64d6e7d2410d0a922d
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-04-24 18:57:58 -0300

    exemplo de encapsulamento e construtores

---
+++ Encapsulamento/Mensalidade/Mensalidade/Entities/Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensalidade.Entities
+{
+    internal class Aluno
+    {
+        public int ID { get; set; } 
+        public String Nome { get; set; }
+        public int Idade { get; set; }
+        public double Mensalidade { get; set; }
+
+        public Aluno()
+        {
+        }
+
+        public Aluno(int id, String nome, int idade, double mens)
+        {
+            ID = id;
+            Nome = nome;
+            Idade = idade;
+            Mensalidade = mens;
+        }
+
+        public double CalcularMensalidade(double desconto)
+        {
+            return this.Mensalidade - (this.Mensalidade * desconto / 100);
+        }
+
+        public override string ToString()
+        {
+            return "ID: "
+                + this.ID
+                + "\nNome: "
+                + this.Nome
+                + "\nIdade: "   
+                + this.Idade
+                + "\nMensalidade: "
+                + this.Mensalidade.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Encapsulamento/Mensalidade/Mensalidade/Program.cs
@@ -0,0 +1,30 @@
+﻿using Mensalidade.Entities;
+using System;
+using System.Globalization;
+
+namespace Mensalidade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            a.ID = 1;
+            Console.Write("Digite seu nome: ");
+            a.Nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            a.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua mensalidade: ");
+            a.Mensalidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double novaMens = a.CalcularMensalidade(10);
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(a.ToString());
+            Console.WriteLine("Mensalidade com desconto: " + novaMens);
+        }
+    }
+}
