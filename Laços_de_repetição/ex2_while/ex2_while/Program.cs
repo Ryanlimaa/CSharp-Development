98f6bfe0f9f801a42b7a5462748c4258493fda0e
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-04-25 17:33:09 -0300

    exemplos de laços de repetições

---
+++ Laços_de_repetição/ex2_while/ex2_while/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex2_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = " ";
+
+            while (opcao != "s")
+            {
+                Console.WriteLine("\n--- Menu ---");
+                Console.WriteLine("Digite 'a' para ver informações do aluno");
+                Console.WriteLine("Digite 'p' para ver informações do professor");
+                Console.WriteLine("Digite 's' para encerrar");
+                Console.Write("Escolha uma opção: ");
+
+                opcao = Console.ReadLine().ToLower();
+
+                switch (opcao)
+                {
+                    case "a":
+                        Console.WriteLine("\nInformações do Aluno: Ryan, ADS, 4° Semestre.");
+                        break;
+                    case "p":
+                        Console.WriteLine("\nInformações do Professor: Prof. Silva, Doutor em Computação.");
+                        break;
+                    case "s":
+                        Console.WriteLine("\nEncerrando programa...");
+                        break;
+                    default:
+                        Console.WriteLine("\nOpção inválida. Tente novamente.");
+                        break;  
+                }
+            }
+        }
+    }
+}
